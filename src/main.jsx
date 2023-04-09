dcec0004c190ac82a4183d2b6952812e1c42e65c
mh akash <mhakash5000@gmail.com>
2023-04-10 00:16:15 +0600

    added navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Home from './components/Home.jsx/Home';
+import Statistics from './components/Statistics/Statistics';
+import Job from './components/Job/Job';
+import Blog from './components/Blog/Blog';
+import NotFound from './components/NotFound';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "statistics",
+        element: <Statistics />,
+      },
+      {
+        path: "job",
+        element: <Job />,
+      },
+      {
+        path: "blog",
+        element: <Blog />,
+      },
+      {
+        path: "*",
+        element: <NotFound/>,
+      },
+    ],
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
+
