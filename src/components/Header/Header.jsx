9d5ab17753d20351de8a00c16a9c32a3b6bc8a65
mh akash <mhakash5000@gmail.com>
2023-04-10 01:05:29 +0600

    header update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,152 +1,140 @@
-import React, { useState } from "react";
-import Logo from "../../../public/logo.png";
-import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Header = () => {
-  const location = useLocation();
-  const [isOpen, setIsOpen] = useState(false);
+function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
   return (
-    <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-center h-16">
-          <div className="flex-shrink-0">
-            <Link to="/">
-              <img className="h-8 w-8" src={Logo} alt="Logo" />
-            </Link>
-          </div>
-          <div className="hidden md:block">
-            <div className="ml-10 flex items-baseline space-x-4">
-              <Link
-                to="/"
-                className={`text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${
-                  location.pathname === "/"
-                    ? "bg-yellow-500 text-white"
-                    : "text-gray-300"
-                }`}
-              >
-                Home
-              </Link>
-              <Link
-                to="/statistics"
-                className={`text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${
-                  location.pathname === "/statistics"
-                    ? "bg-yellow-500 text-white"
-                    : "text-gray-300"
-                }`}
-              >
-                Statistics
-              </Link>
-              <Link
-                to="/job"
-                className={`text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${
-                  location.pathname === "/job"
-                    ? "bg-yellow-500 text-white"
-                    : "text-gray-300"
-                }`}
-              >
-                Applied Jobs
-              </Link>
-              
-              <Link
-                to="/blog"
-                className={`text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${
-                  location.pathname === "/blog"
-                    ? "bg-yellow-500 text-white"
-                    : "text-gray-300"
-                }`}
-              >
-                Blog
-              </Link>
-            </div>
-          </div>
-          <div className="-mr-2 flex md:hidden">
+    <nav className=" text-black">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <button
-              onClick={toggleMenu}
               type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
-              <svg
-                className={`${isOpen ? "hidden" : "block"} h-6 w-6`}
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              <svg
-                className={`${isOpen ? "block" : "hidden"} h-6 w-6`}
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              <span className="sr-only">Open main menu</span>
+              {isMenuOpen ? (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
             </button>
           </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center">
+              <h1 className="  block lg:hidden h-8 w-auto">
+                CodingDuck
+              </h1>
+              <h1 className="  hidden lg:block h-8 w-auto">
+                CodingDuck
+              </h1>
+
+            </div>
+            <div className="hidden sm:block sm:ml-6 flex-grow">
+              <div className="flex justify-center">
+                <Link
+                  to="/"
+                  className="text-gray-600 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Home
+                </Link>
+                <Link
+                  to="/statistics"
+                  className="text-gray-600 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Statistics
+                </Link>
+                <Link
+                  to="/job"
+                  className="text-gray-600 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Applied Jobs
+                </Link>
+                <Link
+                  to="/blog"
+                  className="text-gray-600 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Blog
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="hidden sm:block sm:ml-6">
+            {" "}
+            <div className="flex items-center">
+              <button
+                type="button"
+                className="bg-indigo-400 text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              >
+                Star Applying
+              </button>
+            </div>
+          </div>
         </div>
-        <div className={`${isOpen ? "block" : "hidden"} md:hidden`}>
-          <div className="px-2 pt-2 pb-3 sm:px-3">
-            {/* mobile items */}
+      </div>
+
+      {isMenuOpen && (
+        <div className="sm:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1">
             <Link
               to="/"
-              className={`text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${
-                location.pathname === "/"
-                  ? "bg-yellow-500 text-white"
-                  : "text-gray-300"
-              }`}
+              className="text-gray-600 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
             >
               Home
             </Link>
             <Link
-                to="/statistics"
-                className={`text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${
-                  location.pathname === "/statistics"
-                    ? "bg-yellow-500 text-white"
-                    : "text-gray-300"
-                }`}
-              >
-                Statistics
-              </Link>
-              <Link
-                to="/job"
-                className={`text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${
-                  location.pathname === "/job"
-                    ? "bg-yellow-500 text-white"
-                    : "text-gray-300"
-                }`}
-              >
-                Applied Jobs
-              </Link>
-              
-              <Link
-                to="/blog"
-                className={`text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium ${
-                  location.pathname === "/blog"
-                    ? "bg-yellow-500 text-white"
-                    : "text-gray-300"
-                }`}
-              >
-                Blog
-              </Link>
+              to="/statistics"
+              className="text-gray-600 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Statistics
+            </Link>
+            <Link
+              to="/job"
+              className="text-gray-600 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Applied Jobs
+            </Link>
+            <Link
+              to="/blog"
+              className="text-gray-600 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Blog
+            </Link>
           </div>
         </div>
-      </div>
+      )}
     </nav>
   );
-};
+}
 
-export default Header;
+export default Navbar;
