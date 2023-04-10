5e62ae8d4281587fd4506a0c8a086f7449486ec4
mh akash <mhakash5000@gmail.com>
2023-04-10 11:10:50 +0600

    added statitics

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-import user from './assets/Images/ak.jpg';
+import { useState } from "react";
+import "./App.css";
+import user from "./assets/Images/ak.jpg";
+import JobCategory from "./components/JobCategory/JobCategory";
 
 function App() {
- 
   return (
-    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap:6 my-10 items-center">
-      <div className="text-justify me-3">
-        <p className="text-3xl font-bold">One Step</p>
-        <p className="text-3xl font-bold">Closer To Your</p>
-        <p className="text-blue-400 text-3xl font-bold mb-5">Dream Job</p>
+    <div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap:6 my-10 items-center">
+        <div className="text-justify me-3">
+          <p className="text-3xl font-bold">One Step</p>
+          <p className="text-3xl font-bold">Closer To Your</p>
+          <p className="text-blue-400 text-3xl font-bold mb-5">Dream Job</p>
 
-        <p>
-          Explore thousands of job opportunities with all the information you
-          need. Its your future. Come find it. Manage all your job application
-          from start to finish.
-        </p>
-        <button className="my-5 bg-blue-400 text-white rounded-md p-2">
-          Get Started
-        </button>
+          <p>
+            Explore thousands of job opportunities with all the information you
+            need. Its your future. Come find it. Manage all your job application
+            from start to finish.
+          </p>
+          <button className="my-5 bg-blue-400 text-white rounded-md p-2">
+            Get Started
+          </button>
+        </div>
+        <div>
+          <img className="object-cover rounded-lg" src={user} alt="" />
+        </div>
       </div>
-      <div>
-        <img className=" object-cover" src={user} alt="" />
-      </div>
+      <JobCategory />
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,48 @@
+import { CChart } from '@coreui/react-chartjs';
+
 import React from 'react';
 
 const Statistics = () => {
     return (
-      <div>
-        <h1>statistics</h1>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 my-10 items-center justify-center">
+        <div className='text-center'>
+          <h1>Name: Assignment -9</h1>
+          <h1>Total Mark: 60</h1>
+          <h1>Student ID: WEB7-1405</h1>
+        </div>
+        <div>
+          <CChart
+            className="w-[75%]"
+            type="doughnut"
+            data={{
+              labels: [
+                "Assignment-1",
+                "Assignment-2",
+                "Assignment-3",
+                "Assignment-4",
+                "Assignment-5",
+                "Assignment-6",
+                "Assignment-7",
+                "Assignment-8",
+              ],
+              datasets: [
+                {
+                  backgroundColor: [
+                    "#41B883",
+                    "#E46651",
+                    "#00D8FF",
+                    "#DD1B16",
+                    "#FF5733",
+                    "#DBFF33",
+                    "#75FF33",
+                    "#33FF57",
+                  ],
+                  data: [57, 53, 60, 60, 50, 48, 60, 59],
+                },
+              ],
+            }}
+          />
+        </div>
       </div>
     );
 };
